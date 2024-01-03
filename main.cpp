f1ed601321b1088023cf11d925f425055fc5781b
sathmipeiris <sathmipeiris@gmail.com>
2024-01-03 10:45:31 +0530

    Bank class implementation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include<iostream> 
  
  using namespace std;
+
+ class Bank{
+   protected:
+   string username;
+   string password;
+
+   public:
+   Bank(string username,string password) {
+      this->username =(username);
+      this->password=(password);
+   }
+ };
+
  int main(){
 
-
+   
 
     return 0;
  }
